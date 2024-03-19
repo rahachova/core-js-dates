6fbb06e18b7304971a2783207000137ae3b99dd8
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-03-19 23:26:47 +0100

    feat: add some solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -82,8 +82,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date.getTime());
+  result.setDate(date.getDate() + ((12 - date.getDay()) % 7));
+  return result;
 }
 
 /**
@@ -97,8 +99,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -112,8 +114,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const date1 = new Date(dateStart);
+  const date2 = new Date(dateEnd);
+  const difference = date2.getTime() - date1.getTime();
+  return Math.round(difference / (1000 * 3600 * 24)) + 1;
 }
 
 /**
@@ -133,8 +138,14 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkDate = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+  return (
+    checkDate.getTime() <= endDate.getTime() &&
+    checkDate.getTime() >= startDate.getTime()
+  );
 }
 
 /**

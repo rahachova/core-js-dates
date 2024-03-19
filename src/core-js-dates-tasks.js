261b83b9e6a4f928ca2ded91e4de43916c3679cc
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-03-19 22:00:41 +0100

    feat: add some decisions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,9 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const endData = new Date(date);
+  return endData.getTime();
 }
 
 /**
@@ -31,8 +32,9 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const x = new Date(date);
+  return `${x.getHours().toString().padStart(2, '0')}:${x.getMinutes().toString().padStart(2, '0')}:${x.getSeconds().toString().padStart(2, '0')}`;
 }
 
 /**
@@ -46,8 +48,27 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const x = new Date(date);
+  const day = x.getDay();
+  switch (day) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+      return '';
+  }
 }
 
 /**
